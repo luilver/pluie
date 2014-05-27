be3fc1b4a91375640506eb359ead8e3ec68e79ea
Luilver Garces <luilver@gmail.com>
2014-05-27 16:14:50 -0400

    Group messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :groups
+
   resources :single_messages
 
   devise_for :users
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140527190812) do
+ActiveRecord::Schema.define(version: 20140527200812) do
+
+  create_table "contacts", force: true do |t|
+    t.string   "name"
+    t.integer  "gsm_number_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "groups", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "gsm_numbers", force: true do |t|
     t.string   "number"
