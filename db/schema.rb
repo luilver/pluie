97ade6ceec4e1c08e471f7c78ac5a5d0286f309d
Luilver Garces <luilver@gmail.com>
2014-05-31 14:45:33 -0400

    Admin users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140531165400) do
+ActiveRecord::Schema.define(version: 20140531184406) do
 
   create_table "bulk_messages", force: true do |t|
     t.text     "message"
@@ -85,6 +85,7 @@ ActiveRecord::Schema.define(version: 20140531165400) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
