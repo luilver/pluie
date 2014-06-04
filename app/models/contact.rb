6ac9d260b1a41fa6596f9496849b3fef4d838649
Luilver Garces <luilver@gmail.com>
2014-06-04 14:44:42 -0400

    Groups/contacts relation

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,9 @@
 class Contact < ActiveRecord::Base
   belongs_to :user
+  has_and_belongs_to_many :groups
+
+  def to_s
+    self.name
+  end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140531184406) do
+ActiveRecord::Schema.define(version: 20140604182610) do
 
   create_table "bulk_messages", force: true do |t|
     t.text     "message"
@@ -27,6 +27,11 @@ ActiveRecord::Schema.define(version: 20140531184406) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+  end
+
+  create_table "contacts_groups", id: false, force: true do |t|
+    t.integer "group_id"
+    t.integer "contact_id"
   end
 
   create_table "credits", force: true do |t|
