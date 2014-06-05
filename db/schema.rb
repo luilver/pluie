926a4a314146befcc713a2400c9f185b6ed73cd4
Luilver Garces <luilver@gmail.com>
2014-06-05 12:12:37 -0400

    Remove list_id to bulk_message

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140605155516) do
+ActiveRecord::Schema.define(version: 20140605160851) do
 
   create_table "bulk_messages", force: true do |t|
     t.text     "message"
-    t.integer  "list_id"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
