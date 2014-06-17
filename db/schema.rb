7bd1588dffa64cf12ae6bc4208638ad01ebe9d25
José Javier Señaris <pepesenaris@gmail.com>
2014-06-17 18:19:22 -0400

    migrate delivery reports

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140617121250) do
+ActiveRecord::Schema.define(version: 20140619145443) do
 
   create_table "bulk_messages", force: true do |t|
     t.text     "message"
