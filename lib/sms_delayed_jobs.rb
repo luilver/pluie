74d603f41552b7d6f98c422a9164baa06e7a769d
José Javier Señaris <pepesenaris@gmail.com>
2014-06-17 17:29:10 -0400

    changed single message job class

--- lib/sms_delayed_jobs.rb
+++ lib/sms_delayed_jobs.rb
@@ -1,14 +1,18 @@
 module SmsApi
 
-    SingleMessageJob = Struct.new(:dispatcher, :single_msg) do
+    class SingleMessageJob
 
-        def perform
-          if single_msg.gsm_numbers.count == 1
-            dispatcher.send_single_message(single_msg.gsm_numbers.first.number, single_msg.message)
+      def initialize(dispatcher, single_msg)
+        @dispatcher = dispatcher
+        @single_msg = single_msg
+      end
+
+      def perform
+          if @single_msg.gsm_numbers.count == 1
+            @dispatcher.send_single_message(@single_msg.gsm_numbers.first.number, single_msg.message)
           else
-            dispatcher.send_multiple_messages(single_msg.gsm_numbers.map { |n| n.number  }, single_msg.message)
+            @dispatcher.send_multiple_messages(@single_msg.gsm_numbers.map { |n| n.number  }, single_msg.message)
           end
         end
-
     end
 end
