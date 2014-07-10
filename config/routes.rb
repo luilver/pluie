aa14052fa50916bd6ab597d4299c35498a2a7070
José Javier Señaris <pepesenaris@gmail.com>
2014-07-10 16:27:59 -0400

    Generate Gateway scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 require 'api_constraints'
 
 Rails.application.routes.draw do
+  resources :gateways
+
   namespace :api, :defaults => {:format => 'json'} do
     scope :module => :v1, :constraints => ApiConstraints.new(:version => 1, :default => true) do
       get 'user/balance' => 'users#balance'
