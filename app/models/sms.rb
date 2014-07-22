4829a66ed9b0417faebb08a3e43c7462478e45f1
José Javier Señaris <pepesenaris@gmail.com>
2014-07-22 18:32:49 -0400

    Charge sms to user

--- app/models/sms.rb
+++ app/models/sms.rb
@@ -6,12 +6,19 @@ class Sms < ActiveRecord::Base
   MAX_SIZE = 160
 
   def text
+    #TODO
     #devolver la propiedad message de single_message o bulk_message
     #(segun este definido en la relacion polimorfica) pero con el encoding
     #asociado al gateway
+    self.msg.message
   end
 
   def cost
     @cost ||= self.gateway.unit_price * (text.size / MAX_SIZE)
   end
+
+  def charge_to_user
+    self.user.decrease_balance(cost)
+  end
+
 end
