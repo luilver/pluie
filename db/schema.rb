07132a52d8dc6d536c0240e2d23a81fbcc16ea43
Luilver Garces <luilver@gmail.com>
2014-07-25 19:40:13 -0400

    Drop delayed jobs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140722222139) do
+ActiveRecord::Schema.define(version: 20140725233202) do
 
   create_table "api_settings", force: true do |t|
     t.string   "api_key"
@@ -61,22 +61,6 @@ ActiveRecord::Schema.define(version: 20140722222139) do
     t.datetime "updated_at"
     t.integer  "user_id"
   end
-
-  create_table "delayed_jobs", force: true do |t|
-    t.integer  "priority",   default: 0, null: false
-    t.integer  "attempts",   default: 0, null: false
-    t.text     "handler",                null: false
-    t.text     "last_error"
-    t.datetime "run_at"
-    t.datetime "locked_at"
-    t.datetime "failed_at"
-    t.string   "locked_by"
-    t.string   "queue"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority"
 
   create_table "delivery_reports", force: true do |t|
     t.string   "msg_id"
