00339df0b4a4010e803b7ca56eb3566095ed9bb2
José Javier Señaris <pepesenaris@gmail.com>
2014-07-29 13:48:32 -0400

    Add receivers method in messages

--- app/models/bulk_message.rb
+++ app/models/bulk_message.rb
@@ -4,4 +4,8 @@ class BulkMessage < ActiveRecord::Base
   has_and_belongs_to_many :gsm_numbers
   has_many :sms, as: :msg
 
+  def receivers
+    self.gsm_numbers.map {|gsm| gsm.number}
+  end
+
 end
