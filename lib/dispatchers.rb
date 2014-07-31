ebbdd3d76b90005675d110116b8ce65a4a841a10
José Javier Señaris <pepesenaris@gmail.com>
2014-07-31 14:45:27 -0400

    Delete old dispatcher file

--- lib/dispatchers.rb
+++
@@ -1,5 +0,0 @@
-require 'dispatchers/nexmo_http'
-require 'dispatchers/infobip'
-#es necesario que se ejecute el codigo de  las clases que
-#extienden la funcionalidad de dispatchers/base... para que
-#se añadan al diccionario de la clase Base
