7a2ea441326d850a55de13441f96d1c9a39ad5dd
José Javier Señaris <pepesenaris@gmail.com>
2014-08-04 19:12:53 -0400

    EM Async infobip http

--- lib/delivery_methods/dispatchers/infobip.rb
+++ lib/delivery_methods/dispatchers/infobip.rb
@@ -1,31 +1,68 @@
-require_relative 'base'
+require_relative 'async_http'
 require 'oneapi-ruby'
+require 'em-http'
+require 'em-http/middleware/json_response'
 
 module DeliveryMethods
 
-    class Infobip < DeliveryMethods::Base
+    class Infobip < DeliveryMethods::AsyncHttp
 
       register_subclass "infobip_http"
 
       def initialize(user)
         super
         @client = OneApi::SmsClient.new(INFOBIP_KEY, INFOBIP_PASS)
+        @base_url = 'https://oneapi.infobip.com/'
+        @sender_address = cubacel_random_number.to_s
+        @path_url = "/1/smsmessaging/outbound/#{@sender_address}/requests"
+        # en oneapi, de infobip, al parecer identifica al numero que envia el sms
+        #buscar si puede ser aleatorio o no
       end
 
-      def send_single_message(to, text)
-        begin
-          sms = OneApi::SMSRequest.new
-          sms.address =  to
-          sms.message = text
+      def send_sms(sms_list)
+        http = []
 
-          result = client.send_sms(sms)
-          # Store the client correlator to be able to query for the delivery status later:
-          client_correlator = result.client_correlator
-          return true
-        rescue Exception => e
-          log_error("Failed sending msg to: #{to}. #{e.message}")
-          return false
+        EM.run do
+
+          sms_list.each_with_index do |sms, index|
+            params = get_params_from_sms(sms)
+            options = request_options(nil, params)
+            conn =  EM::HttpRequest.new(base_url, connection_options)
+            conn.use EM::Middleware::JSONResponse
+            http << conn.post(options)
+            h = http[index]
+            h.callback do
+              msg_id = h.response['clientCorrelator']
+              log_info("Sended msg: #{msg_id} using infobip_http" )
+            end
+
+            h.errback do
+              #Log error
+            end
+
+
+          end
         end
+
       end
+
+      def get_params_from_sms(sms)
+        {
+          :senderAddress => @senderAddress,
+          :address => sms.receiver,
+          :message => sms.text,
+          :clientCorrelator => sms.id.to_s,
+        }
+            #params['notifyURL'] = sms.notify_url  #buscar, pero debe ser para definir una url a donde dar el delivery report
+      end
+
+      protected
+        def request_options(query, body, keepalive= false)
+          options = super(query, body, keepalive)
+          head = options[:head] || {}
+          head[:authorization] = [INFOBIP_KEY, INFOBIP_PASS]
+          options[:head] = head
+          options
+        end
   end
 end
