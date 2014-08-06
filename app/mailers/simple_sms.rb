fb7db16c92ca4ac3ffbbe34ad8d8bd198829f26b
José Javier Señaris <pepesenaris@gmail.com>
2014-08-06 12:27:59 -0400

    Simple sms of ActionSmser::Base

---
+++ app/mailers/simple_sms.rb
@@ -0,0 +1,10 @@
+class SimpleSms < ActionsSmser::Base
+  def multiple_receivers(receivers, text, dlr_method=nil)
+    if dlr_method && ActionsSmser.delivery_options[dlr_method]
+      #update delivery method  for this sms.
+      delivery_options[:delivery_method] = dlr_method
+    end
+    sms(:to => receivers, :from => "", :body => text)
+  end
+
+end
