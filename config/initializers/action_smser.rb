042601fc142c2ac8eda7c8680d2e9bab909992c5
José Javier Señaris <pepesenaris@gmail.com>
2014-08-07 16:17:30 -0400

    Use Async infobip as delivery method

--- config/initializers/action_smser.rb
+++ config/initializers/action_smser.rb
@@ -1,8 +1,12 @@
+NEXMO_KEY = ENV['NEXMO_API_KEY']
+NEXMO_PASS = ENV['NEXMO_API_SECRET']
+
 if Rails.env.development? || Rails.env.production?
+  require 'action_smser/delivery_methods/async_infobip'
 
   ActionSmser.delivery_options[:delivery_method] = :async_infobip
-  #ActionSmser.delivery_options[:delivery_method] = :delayed_job
-  #ActionSmser.delivery_options[:smstrade] = {      :username => 'username', :password => 'password' }
-  #ActionSmser.delivery_options[:delayed_job] = { :delivery_method => :smstrade, :priority => 0 }
+  #ActionSmser.delivery_options[:delivery_method] = :nexmo
+  #ActionSmser.delivery_options[:nexmo] = {:username => NEXMO_KEY, :password => NEXMO_PASS }
+  #ActionSmser.delivery_options[:delayed_job] = { :delivery_method => :nexmo, :priority => 0 }
   # ActionSmser.delivery_options[:save_delivery_reports] = true
 end
