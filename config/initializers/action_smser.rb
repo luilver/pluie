51ccd9d0ab339db91fd65a4cbb7ccedc40295e7e
José Javier Señaris <pepesenaris@gmail.com>
2014-08-11 18:42:38 -0400

    Load changes to ActionSmser::DeliveryReport

--- config/initializers/action_smser.rb
+++ config/initializers/action_smser.rb
@@ -1,5 +1,6 @@
 if Rails.env.development? || Rails.env.production?
   require 'action_smser_delivery_methods' #load the DeliveryMethods implemented in this app
+  require 'action_smser_utils' #to modify the ActionSmser::DeliveryReport model
 
   ActionSmser.delivery_options[:delivery_method] = :async_infobip
   #ActionSmser.delivery_options[:delivery_method] = :nexmo
