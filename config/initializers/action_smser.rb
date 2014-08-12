a979f44810fb1bef8484befc50f371fdefde3ae0
José Javier Señaris <pepesenaris@gmail.com>
2014-08-12 12:49:49 -0400

    Save DLRs with async infobip

--- config/initializers/action_smser.rb
+++ config/initializers/action_smser.rb
@@ -6,5 +6,6 @@ if Rails.env.development? || Rails.env.production?
   #ActionSmser.delivery_options[:delivery_method] = :nexmo
   #ActionSmser.delivery_options[:nexmo] = {:username => NEXMO_KEY, :password => NEXMO_PASS }
   #ActionSmser.delivery_options[:delayed_job] = { :delivery_method => :nexmo, :priority => 0 }
-  # ActionSmser.delivery_options[:save_delivery_reports] = true
+   ActionSmser.delivery_options[:save_delivery_reports] = true
+   ActionSmser.delivery_options[:gateway_commit]['infobip'] = ActionSmser::DeliveryMethods::AsyncInfobip
 end
