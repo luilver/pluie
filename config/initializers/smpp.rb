9eb5825e7f9ec7f10933a3164a70c9308dc9bfc5
José Javier Señaris <pepesenaris@gmail.com>
2014-08-12 14:40:04 -0400

    Remove old msg queue in smpp

--- config/initializers/smpp.rb
+++ config/initializers/smpp.rb
@@ -5,47 +5,6 @@ require 'smpp_tools/producer/simple_producer'
 
 module PluieEM
 
-  @@msg_queue = EM::Queue.new
-  @@configs = {}
-
-  def self.queue
-    @@msg_queue
-  end
-
-  def self.enqueue(sms)
-    queue << sms
-  end
-
-  def self.gateway_config(gateway_name)
-    config = {
-    :host => 'smpp3.infobip.com',
-    :port => 8888,
-    :system_id => INFOBIP_KEY,
-    :password => INFOBIP_PASS,
-    :system_type => '', # default given according to SMPP 3.4 Spec
-    :interface_version => 52,
-    :source_ton  => 0,
-    :source_npi => 1,
-    :destination_ton => 1,
-    :destination_npi => 1,
-    :source_address_range => '',
-    :destination_address_range => '',
-    :enquire_link_delay_secs => 30
-  }
-
-  @@configs[:infobip] = config
-
-  @@configs[gateway_name]
-
-  end
-
-  def self.run_em
-
-    smpp = SimpleSmppGateway.new(:infobip, queue, Rails.logger)
-    config =  gateway_config(smpp.name)
-    smpp.start_loop(config)
-  end
-
   def run_loop
     loop do
       EM.run
