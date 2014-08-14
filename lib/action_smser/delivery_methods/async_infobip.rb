3f1ab548f4140fbce04a81e79fcbc1cd334438a3
José Javier Señaris <pepesenaris@gmail.com>
2014-08-14 11:17:27 -0400

    Build request with string keys

--- lib/action_smser/delivery_methods/async_infobip.rb
+++ lib/action_smser/delivery_methods/async_infobip.rb
@@ -14,17 +14,17 @@ module ActionSmser::DeliveryMethods
     @sender_address = ""
     @host = 'api.infobip.com'
     @base_url = "http://#{@host}/"
-    @path_url = "/api/v3/sendsms/json"
+    @path_url = "api/v3/sendsms/json"
 
     class << self
       attr_reader :sender_address, :host
     end
 
     def self.deliver(sms)
-      r_head = {:host => host, 'content-type' => 'application/json'}
+      r_head = {"host" => host, 'content-type' => 'application/json', "accept" => "*/*"}
       dest = {}# to associates each recipient or destination to the generated messageId
       info = self.sms_info(sms, dest)
-      r_body = {authentication: {username: INFOBIP_KEY, password: INFOBIP_PASS }, messages: [info]}
+      r_body = {"authentication" => {"username" => INFOBIP_KEY, "password" => INFOBIP_PASS }, "messages" => [info]}
       r_body = r_body.to_json
       conn_options = self.connection_options()
 
@@ -32,12 +32,11 @@ module ActionSmser::DeliveryMethods
 
       EM.run do
         connection = EM::HttpRequest.new(base_url, conn_options)
-        options = self.request_options(r_head, nil, r_body)
-        http = connection.post(options)
+        http = connection.post(:head => r_head, :body => r_body, :path => path_url, :keepalive => false)
 
         http.callback do
           if sms.delivery_options[:save_delivery_reports]
-            results = JSON.parse(http.response["results"])
+            results = JSON.parse(http.response)["results"] rescue nil
             if results
               self.save_delivery_reports(sms, results, dest)
             else
@@ -59,13 +58,13 @@ module ActionSmser::DeliveryMethods
       recipients = []
       sms.to_numbers_array.each do |number|
         id = SecureRandom.uuid()
-        recipients << {gsm: number, messageId: id }
+        recipients << {"gsm" => number, "messageId" => id }
         dest[id] = number
       end
 
-      msg = {sender: sender_address, text: sms.body, recipients: recipients}
+      msg = { "text" => sms.body, "recipients" => recipients}
       if ActionSmser::Base.message_real_length(sms.body) > 160
-        msg[:type] = "longSMS"
+        msg["type"] = "longSMS"
       end
       msg
     end
@@ -89,12 +88,7 @@ module ActionSmser::DeliveryMethods
       INFOBIP_HTTP_ERROR_CODES[error_code] || "UNKNOWN_ERROR"
     end
 
-    def self.cubacel_random_number
-        result = "535"
-        rand = Random.new
-        7.times{ result << rand.rand(9).to_s}
-        result
-    end
+
 
   end
 end
