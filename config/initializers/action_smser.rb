0199b000de32eef94aeafd1b1d5ad832481c2e36
José Javier Señaris <pepesenaris@gmail.com>
2014-08-14 13:38:18 -0400

    Move credentials loading to action_smser intializer

--- config/initializers/action_smser.rb
+++ config/initializers/action_smser.rb
@@ -1,3 +1,11 @@
+#infobip -> oneapi
+INFOBIP_KEY = ENV['INFOBIP_KEY']
+INFOBIP_PASS = ENV['INFOBIP_PASS']
+
+#nexmo -> enabled for smpp use
+NEXMO_KEY = ENV['NEXMO_API_KEY']
+NEXMO_PASS = ENV['NEXMO_API_SECRET']
+
 if Rails.env.development? || Rails.env.production?
   require 'action_smser_delivery_methods' #load the DeliveryMethods implemented in this app
   require 'action_smser_utils' #to modify the ActionSmser::DeliveryReport model
@@ -5,4 +13,5 @@ if Rails.env.development? || Rails.env.production?
   ActionSmser.delivery_options[:delivery_method] = :async_infobip
   ActionSmser.delivery_options[:save_delivery_reports] = true
   ActionSmser.delivery_options[:gateway_commit]['infobip'] = ActionSmser::DeliveryMethods::AsyncInfobip
+  ActionSmser.delivery_options[:infobip] = {unit_price: 0.01, username: INFOBIP_KEY, password: INFOBIP_PASS}
 end
