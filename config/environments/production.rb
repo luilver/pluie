d7c8a4a10f88b0b95cab261572ad33e7b77e6eef
Luilver Garces <luilver@gmail.com>
2014-08-15 15:49:25 -0400

    Fix rails 4 configure

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Rails.application.configure do
+Pluie::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
