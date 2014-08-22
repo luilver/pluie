2d2604b118fb85428b2583f633764ce83cd87d9b
José Javier Señaris <pepesenaris@gmail.com>
2014-08-22 16:25:21 -0400

    Fix return value in save DLRs

--- lib/action_smser/delivery_methods/async_infobip.rb
+++ lib/action_smser/delivery_methods/async_infobip.rb
@@ -89,6 +89,9 @@ module ActionSmser::DeliveryMethods
         end
       rescue Exception => e
         ActionSmser::Logger.error "Fail saving DLRs. #{e.message}.\n Trace: #{e.backtrace.join("\n")}"
+      ensure
+        count
+      end
     end
 
     def self.process_delivery_report(params)
