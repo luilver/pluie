f6c6d6a400e714f3b311c306128e39e89c38fe28
José Javier Señaris <pepesenaris@gmail.com>
2014-08-25 13:32:07 -0400

    Fix params in build_with_user

--- lib/action_smser_utils/user_to_delivery_report.rb
+++ lib/action_smser_utils/user_to_delivery_report.rb
@@ -8,8 +8,8 @@ module ActionSmserUtils
 
 
     module ClassMethods
-      def build_with_user(sms, to, msg_id, user)
-        dr = ActionSmser::DeliveryReport.build_from_sms(sms, to, msg_id, route_name)
+      def build_with_user(sms, to, msg_id, user, route_name)
+        dr = ActionSmser::DeliveryReport.build_from_sms(sms, to, msg_id)
         dr.user = user
         dr.gateway = route_name
         dr
