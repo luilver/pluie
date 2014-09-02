595406670b01a75742470aed019782932b8d5b58
José Javier Señaris <pepesenaris@gmail.com>
2014-09-02 16:57:25 -0400

    Methods to queue bulk sms jobs

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -3,3 +3,11 @@ Delayed::Worker.max_attempts = 1
 #If you want to keep failed jobs, set Delayed::Worker.destroy_failed_jobs = false.
 #The failed jobs will be marked with non-null failed_at.
 Delayed::Worker.destroy_failed_jobs = false
+
+def bulk_sms_priority(index)
+  index
+end
+
+def bulk_sms_queue
+  "bulk_sms"
+end
