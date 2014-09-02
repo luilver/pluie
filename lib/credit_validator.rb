e09d996e80ea23ebdb9a6a5fb3417aaca7babb0e
José Javier Señaris <pepesenaris@gmail.com>
2014-09-02 12:40:32 -0400

    Change credit validation msg

--- lib/credit_validator.rb
+++ lib/credit_validator.rb
@@ -4,7 +4,7 @@ module Validations
       cost = record.receivers.size * record.route.price
       cl = record.user.credit_limit
       if (record.user.balance - cost) < cl
-        record.errors[:base] << "Message cost(#{cost}) is greater than your credit limit (#{cl})"
+        record.errors[:base] << "Message cost(#{cost}) violates your credit limit (#{cl})"
       end
     end
   end
