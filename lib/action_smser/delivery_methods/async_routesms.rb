a93840d1f6e8e3982c5674c961c0e0056fabeaa6
José Javier Señaris <pepesenaris@gmail.com>
2014-09-05 15:51:38 -0400

    Route sms delivery method

---
+++ lib/action_smser/delivery_methods/async_routesms.rb
@@ -0,0 +1,38 @@
+require_relative 'async_http'
+
+module ActionSmser::DeliveryMethods
+  class AsyncRoutesms < AsyncHttp
+    #:path_url, :base_url, :r_head, :r_body, :gateway_key
+    @path_url = "bulksms/bulksms"
+    @base_url = "http://smpp2.routesms.com:8080/"
+    #@r_head =  nil # ver si basta con no declarlo
+    @gateway_key = :routesms
+
+    def self.sms_info(sms)
+      msg = {
+        :username => sms.delivery_options[gateway_key][:username],
+        :password => sms.delivery_options[gateway_key][:password],
+        :type => 0,
+        :dlr => 1,
+        :source => cubacel_random_number,
+        :message => sms.body
+      }
+      msg
+    end
+
+    def self.request_params(info, numbers, sms)
+      destination =  numbers.join(",")
+      info.merge({:destination => destination})
+    end
+
+    def self.parse_response(response)
+      #devolver array de results
+      ActionSmser::Logger.info "Routesms Response: #{response}"
+    end
+
+    def self.save_delivery_reports(sms, results, user, route_name)
+      0
+    end
+
+  end
+end
