9eeaaadcb76120a5335891ef2e19aa2b95a62675
Luilver Garces <luilver@gmail.com>
2014-09-05 09:52:57 -0400

    Re-sent instead of updated

--- app/controllers/bulk_messages_controller.rb
+++ app/controllers/bulk_messages_controller.rb
@@ -54,7 +54,7 @@ class BulkMessagesController < ApplicationController
   def update
     respond_to do |format|
       if @bulk_message.update(bulk_message_params)
-        format.html { redirect_to @bulk_message, notice: 'Bulk message was successfully updated.' }
+        format.html { redirect_to @bulk_message, notice: 'Bulk message was successfully re-sent.' }
         format.json { render :show, status: :ok, location: @bulk_message }
       else
         format.html { render :edit }
