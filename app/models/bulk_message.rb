445e9ea6e8450fed8f5f76293ce78f8220272790
Luilver Garces <luilver@gmail.com>
2014-09-09 16:41:12 -0400

    Monit and bulk test with DJ

--- app/models/bulk_message.rb
+++ app/models/bulk_message.rb
@@ -16,6 +16,7 @@ class BulkMessage < ActiveRecord::Base
   end
 
   def deliver
+    begin
     dlr_method = self.route.gateway.name
     numbers = receivers.to_a
     size = [(numbers.size * ActionSmser.delivery_options[:numbers_from_bulk]).to_i, ActionSmser.delivery_options[:min_numbers_in_sms]].max
@@ -23,6 +24,14 @@ class BulkMessage < ActiveRecord::Base
     batches.each_with_index do |nums, index|
       sms = SimpleSms.multiple_receivers(nums, self.message, self.user.id, self.route.id, dlr_method)
       Delayed::Job.enqueue(sms, :priority => bulk_sms_priority(index), :queue => bulk_sms_queue)
+    end
+    rescue StandardError => e
+      File.open('/tmp/delayed_job.log', "a+") do |file|
+        file.write "Dentro del rescue \n"
+      end
+    end
+    File.open('/tmp/delayed_job.log', "a+") do |file|
+      file.write "Fuera del rescue \n"
     end
   end
 
