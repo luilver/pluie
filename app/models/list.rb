053b132e6ce77fec6d04bdba1bca8d4ad52fa098
Luilver Garces <luilver@gmail.com>
2014-09-10 09:38:59 -0400

    Validate List name and file

--- app/models/list.rb
+++ app/models/list.rb
@@ -4,6 +4,8 @@ class List < ActiveRecord::Base
   has_and_belongs_to_many :bulk_messages
   has_attached_file :file
 
+  validates :name, presence: true
+  validates_presence_of :file, :message => "can't be blank (only .txt supported)"
   validates_attachment :file, content_type: { content_type: ["text/plain"]}
 
 end
