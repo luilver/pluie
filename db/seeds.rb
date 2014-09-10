b193735777b9fadbdf873bfd44a1009925ce981c
Luilver Garces <luilver@gmail.com>
2014-09-10 10:57:46 -0400

    Seed users, gateways and routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Luilver
+u = User.create([{ email: 'luilver@gmail.com', password: 'luilver8' },
+                 { email: 'pepesenaris@gmail.com', password: 'pepe5678' }])
+
+# Routesms
+g = Gateway.new
+g.name = 'routesms'
+g.price = 0.0035
+g.save
+r = Route.new
+r.price = 0.008
+r.name = "Plata"
+r.user = User.first
+r.gateway = g
+r.save
+r = Route.new
+r.price = 0.008
+r.name = "Silver"
+r.user = User.last
+r.gateway = g
+r.save
+
+# Infobip
+g = Gateway.new
+g.name = 'infobip'
+g.price = 0.0055
+g.save
+r = Route.new
+r.price = 0.01
+r.name = "Oro"
+r.user = User.first
+r.gateway = g
+r.save
+r = Route.new
+r.price = 0.01
+r.name = "Gold"
+r.user = User.last
+r.gateway = g
+r.save
