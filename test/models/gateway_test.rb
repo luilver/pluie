f75732f927d1cf0d4fdd641837114fbd0387d0e4
Luilver Garces <luilver@gmail.com>
2014-09-15 16:34:46 -0400

    Gateway should have price

--- test/models/gateway_test.rb
+++ test/models/gateway_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class GatewayTest < ActiveSupport::TestCase
+  test "should have price" do
+    gateway = Gateway.random
+    assert_not gateway.price.blank?
+  end
+
   test "should have name" do
     gateway = Gateway.random
     assert_not gateway.name.blank?
