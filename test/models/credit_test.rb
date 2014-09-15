1b549147144cd1bce930b700bc27a0a7541e351a
Luilver Garces <luilver@gmail.com>
2014-09-15 16:31:46 -0400

    Credit should have user

--- test/models/credit_test.rb
+++ test/models/credit_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class CreditTest < ActiveSupport::TestCase
+  test "should have user" do
+    credit = Credit.random
+    assert_not credit.user.blank?
+  end
+
   test "should have balance" do
     credit = Credit.random
     assert_not credit.balance.blank?
