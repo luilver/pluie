5cd6f997c154f5adc4a6a05927c80bd908425288
Luilver Garces <luilver@gmail.com>
2014-09-15 16:44:15 -0400

    Sms should have gateway

--- test/models/sms_test.rb
+++ test/models/sms_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class SmsTest < ActiveSupport::TestCase
+  test "should have gateway" do
+    sms = Sms.random
+    assert_not sms.gateway.blank?
+  end
+
   test "should have msg type" do
     sms = Sms.random
     assert_not sms.msg_type.blank?
