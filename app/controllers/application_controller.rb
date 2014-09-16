53546ece7a77aa69668597707bfceebcd547ba26
José Javier Señaris <pepesenaris@gmail.com>
2014-09-16 15:59:57 -0400

    Set locale & url_options in AppController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,15 @@ class ApplicationController < ActionController::Base
     redirect_to root_url, :alert => exception.message
   end
 
+  before_action :set_locale
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options(options = {})
+    {locale: I18n.locale}
+  end
+
+
 end
