0143bfb550c760c8bef63e77664e44746866bf6c
José Javier Señaris <pepesenaris@gmail.com>
2014-09-16 16:01:28 -0400

    Setup routes to support locales

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 require 'api_constraints'
 
 Rails.application.routes.draw do
-  resources :routes
-
-  resources :gateways
 
   namespace :api, :defaults => {:format => 'json'} do
     scope :module => :v1, :constraints => ApiConstraints.new(:version => 1, :default => true) do
@@ -16,24 +13,38 @@ Rails.application.routes.draw do
     end
   end
 
-  mount ActionSmser::Engine => '/'
-  get 'delivery_reports' => 'action_smser/delivery_reports', as: :delivery_reports
-  get 'delivery_reports/list' => 'action_smser/delivery_reports#list', as: :list_delivery_reports
-  get 'delivery_reports/:id' => 'action_smser/delivery_reports#show', as: :delivery_report
 
-  resources :credits
 
-  resources :bulk_messages
+  scope ":locale", locale: /en|es/ do
 
-  resources :lists
+    mount ActionSmser::Engine => '/'
+    get 'delivery_reports' => 'action_smser/delivery_reports', as: :delivery_reports
+    get 'delivery_reports/list' => 'action_smser/delivery_reports#list', as: :list_delivery_reports
+    get 'delivery_reports/:id' => 'action_smser/delivery_reports#show', as: :delivery_report
 
-  #resources :group_messages
+    resources :routes
 
-  #resources :groups
+    resources :gateways
 
-  #resources :contacts
+    resources :credits
 
-  resources :single_messages
+    resources :bulk_messages
+
+    resources :lists
+
+    #resources :group_messages
+
+    #resources :groups
+
+    #resources :contacts
+
+    resources :single_messages
+  end
+
+  get '/:locale' => "home#index"
+  #get '*path', to: redirect("/#{I18n.default_locale}/%{path}"), constraints: lambda { |req| !req.path.starts_with? "/#{I18n.default_locale}/" }
+  #get '', to: redirect("/#{I18n.default_locale}/home#index")
+
 
   devise_for :users
   root to: "home#index"
