18e425e5f9f5d78ba8d151254a6b837759e2bc59
José Javier Señaris <pepesenaris@gmail.com>
2014-09-17 19:35:01 -0400

    Check user route in Credit validation

--- lib/credit_validator.rb
+++ lib/credit_validator.rb
@@ -1,10 +1,14 @@
 module Validations
   class CreditValidator < ActiveModel::Validator
     def validate(record)
-      cost = record.receivers.size * record.route.price
-      cl = record.user.credit_limit
-      if (record.user.balance - cost) < cl
-        record.errors[:base] << "Message cost(#{cost}) violates your credit limit (#{cl})"
+      unless record.route
+        record.errors[:base] << "#{I18n.t(:not_route_error)}"
+      else
+        cost = record.receivers.size * record.route.price
+        cl = record.user.credit_limit
+        if (record.user.balance - cost) < cl
+          record.errors[:base] << "Message cost(#{cost}) violates your credit limit (#{cl})"
+        end
       end
     end
   end
