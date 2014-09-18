23221c3a08971c3cbd8d371a7463408b4d67e1d6
José Javier Señaris <pepesenaris@gmail.com>
2014-09-17 20:34:33 -0400

    Avoid receivers in CreditValidator

--- lib/credit_validator.rb
+++ lib/credit_validator.rb
@@ -4,7 +4,7 @@ module Validations
       unless record.route
         record.errors[:base] << "#{I18n.t(:not_route_error)}"
       else
-        cost = record.receivers.size * record.route.price
+        cost = record.gsm_numbers_count * record.route.price
         cl = record.user.credit_limit
         if (record.user.balance - cost) < cl
           record.errors[:base] << "Message cost(#{cost}) violates your credit limit (#{cl})"
