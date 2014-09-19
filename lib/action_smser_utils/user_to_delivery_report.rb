318323e63a3b6379193c58efbb4b109f61cd301a
José Javier Señaris <pepesenaris@gmail.com>
2014-09-19 15:42:50 -0400

    Split ActionSmser hacks into different files

--- lib/action_smser_utils/user_to_delivery_report.rb
+++ lib/action_smser_utils/user_to_delivery_report.rb
@@ -1,31 +1,10 @@
 module ActionSmserUtils
-  module ShowDeliveryReportsController
-    extend ActiveSupport::Concern
-
-    included do
-      before_action :set_delivery_report, only: [:show]
-      load_and_authorize_resource except: [:gateway_commit]
-
-      rescue_from CanCan::AccessDenied do |exception|
-        redirect_to main_app.root_url, :alert => exception.message
-      end
-
-      def show
-      end
-
-      def set_delivery_report
-        @delivery_report = ActionSmser::DeliveryReport.find(params[:id])
-      end
-    end
-  end
-
   module UserToDeliveryReport
     extend ActiveSupport::Concern
 
     included do
       belongs_to :user
     end
-
 
     module ClassMethods
       def build_with_user(sms, to, msg_id, user, route_name)
@@ -46,5 +25,3 @@ module ActionSmserUtils
 end
 
 ActionSmser::DeliveryReport.send :include, ActionSmserUtils::UserToDeliveryReport
-
-ActionSmser::DeliveryReportsController.send :include, ActionSmserUtils::ShowDeliveryReportsController
