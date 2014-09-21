d32b575f686d5c27e742f290994bbf1644993351
José Javier Señaris <pepesenaris@gmail.com>
2014-09-21 11:29:55 -0400

    Validates user presence in new Credit

--- app/models/credit.rb
+++ app/models/credit.rb
@@ -2,6 +2,7 @@ class Credit < ActiveRecord::Base
   belongs_to :user
   before_save :save_owner
   validates :balance,  numericality: {only_integer: true, greater_than: 0}
+  validates :user, presence: true
 
   def save_owner
     self.user.save
