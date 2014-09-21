ff77d3e9d994d029d2de13cb991d527d1baff17b
José Javier Señaris <pepesenaris@gmail.com>
2014-09-21 11:16:20 -0400

    I18n for file missing in paperclip

--- app/models/list.rb
+++ app/models/list.rb
@@ -5,7 +5,7 @@ class List < ActiveRecord::Base
   has_attached_file :file
 
   validates :name, presence: true
-  validates_presence_of :file, :message => "can't be blank (only .txt supported)"
+  validates_presence_of :file, :message => I18n.translate(:paperclip_file_presence)
   validates_attachment :file, content_type: { content_type: ["text/plain"]}
 
 end
