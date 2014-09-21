8b6d26214bf69392635f4c84713ff9eddb4eb265
José Javier Señaris <pepesenaris@gmail.com>
2014-09-21 10:40:45 -0400

    I18n for CreditValidation

--- lib/credit_validator.rb
+++ lib/credit_validator.rb
@@ -2,12 +2,12 @@ module Validations
   class CreditValidator < ActiveModel::Validator
     def validate(record)
       unless record.route
-        record.errors[:base] << "#{I18n.t(:not_route_error)}"
+        record.errors[:base] << I18n.t(:not_route_error)
       else
         cost = record.gsm_numbers_count * record.route.price
         cl = record.user.credit_limit
         if (record.user.balance - cost) < cl
-          record.errors[:base] << "Message cost(#{cost}) violates your credit limit (#{cl})"
+          record.errors[:base] << I18n.translate('errors.messages.debit_limit_violation', cost: cost, limit: cl)
         end
       end
     end
