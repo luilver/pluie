4bf3f02c553845a9c8afcd6b50fa2b63072a2e85
José Javier Señaris <pepesenaris@gmail.com>
2014-09-22 18:52:06 -0400

    Attach & Remove numbers from List(DJ)

--- app/models/list.rb
+++ app/models/list.rb
@@ -8,4 +8,24 @@ class List < ActiveRecord::Base
   validates_presence_of :file, :message => I18n.translate(:paperclip_file_presence)
   validates_attachment :file, content_type: { content_type: ["text/plain"]}
 
+  def attach_numbers
+    if self.file.path
+      numbers = IO.foreach(self.file.path).map{ |line| GsmNumber.find_or_create_by(number: line[0,10]) }
+      from_file = Set.new(numbers)
+      new_numbers = from_file - self.gsm_numbers.to_a
+      self.gsm_numbers << new_numbers.to_a
+    end
+  end
+
+  def remove_numbers
+    if self.file.path
+      numbers = IO.foreach(self.file.path).map{ |line| GsmNumber.find_by_number(line[0,10]) }
+      from_file = Set.new(numbers)
+      erase_this = from_file & self.gsm_numbers.to_a
+      self.gsm_numbers.delete(erase_this.to_a)
+    end
+    #IO.foreach(self.file.path) { |line| n = GsmNumber.find_by_number(line[0,10]);
+     #                             self.gsm_numbers.delete(n) if self.gsm_numbers.include?(n)  } if self.file.path
+  end
+
 end
