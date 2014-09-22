8febfe6a9982e5e9b7c2637efe5005b57c6872fb
Luilver Garces <luilver@gmail.com>
2014-09-22 02:03:41 -0400

    User unconfirmed_email

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140922040847) do
+ActiveRecord::Schema.define(version: 20140922060236) do
 
   create_table "action_smser_delivery_reports", force: true do |t|
     t.string   "msg_id"
@@ -204,6 +204,7 @@ ActiveRecord::Schema.define(version: 20140922040847) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
