ec73f17be725b515c4436c2cb30eabcfd15f1d44
José Javier Señaris <pepesenaris@gmail.com>
2014-09-21 22:42:32 -0400

    Fix html parsing errors in credit validation

--- lib/credit_validator.rb
+++ lib/credit_validator.rb
@@ -7,7 +7,7 @@ module Validations
         cost = record.gsm_numbers_count * record.route.price
         cl = record.user.credit_limit
         if (record.user.balance - cost) < cl
-          record.errors[:base] << I18n.translate('errors.messages.debit_limit_violation', cost: cost, limit: cl)
+          record.errors[:base] << I18n.translate('errors.messages.debit_limit_violation', cost: cost, limit: cl).html_safe
         end
       end
     end
