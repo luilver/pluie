e15352b4b02fbdd3a20ae734dbf3ee46743e9852
Luilver Garces <luilver@gmail.com>
2014-09-22 01:55:33 -0400

    Devise confirmable users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140917180355) do
+ActiveRecord::Schema.define(version: 20140922040847) do
 
   create_table "action_smser_delivery_reports", force: true do |t|
     t.string   "msg_id"
@@ -201,8 +201,12 @@ ActiveRecord::Schema.define(version: 20140917180355) do
     t.boolean  "admin"
     t.decimal  "balance",                default: 0.0
     t.decimal  "credit_limit",           default: 0.0
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
