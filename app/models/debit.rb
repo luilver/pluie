5f6bd1b55197edcc74809915f59c7510d4963db4
José Javier Señaris <pepesenaris@gmail.com>
2014-09-23 15:05:01 -0400

    Random method in Debit

--- app/models/debit.rb
+++ app/models/debit.rb
@@ -3,4 +3,8 @@ class Debit < ActiveRecord::Base
   validates :balance, presence: true
   validates :balance,  numericality: {only_integer: true, greater_than: 0}
   validates :user, presence: true
+
+  def self.random
+    Debit.all[rand Credit.count]
+  end
 end
