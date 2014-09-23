11e68f733c1db638e3d2fc9d3e96c55fdd9c2d2d
José Javier Señaris <pepesenaris@gmail.com>
2014-09-23 16:34:59 -0400

    Update user credit on Credit update

--- app/models/credit.rb
+++ app/models/credit.rb
@@ -4,6 +4,7 @@ class Credit < ActiveRecord::Base
   validates :balance, presence: true
   validates :balance,  numericality: {only_integer: true, greater_than: 0}
   validates :user, presence: true
+  around_update :update_user_credit
 
   def save_owner
     self.user.save
@@ -12,4 +13,14 @@ class Credit < ActiveRecord::Base
   def self.random
     Credit.all[rand Credit.count]
   end
+
+  private
+    def update_user_credit
+      old_c = self.balance
+      yield
+      if self.valid?
+        new_c = self.user.credit - old_c + self.balance
+        self.user.update(credit: new_c)
+      end
+    end
 end
