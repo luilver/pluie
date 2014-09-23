1690af4c24d115442e3e3773b0d0e55feb8f7f7a
José Javier Señaris <pepesenaris@gmail.com>
2014-09-23 15:03:54 -0400

    Validations in Debit

--- app/models/debit.rb
+++ app/models/debit.rb
@@ -1,3 +1,6 @@
 class Debit < ActiveRecord::Base
   belongs_to :user
+  validates :balance, presence: true
+  validates :balance,  numericality: {only_integer: true, greater_than: 0}
+  validates :user, presence: true
 end
