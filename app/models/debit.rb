4c18a533ace491e9b914acdd3ebec42b995e2a71
José Javier Señaris <pepesenaris@gmail.com>
2014-09-23 19:33:42 -0400

    Save debit from user

--- app/models/debit.rb
+++ app/models/debit.rb
@@ -3,14 +3,14 @@ class Debit < ActiveRecord::Base
   validates :balance, presence: true
   validates :balance,  numericality: { greater_than: 0}
   validates :user, presence: true
-  after_create :save_user_debit
+  #after_save :save_user_debit
 
   def self.random
     Debit.all[rand Credit.count]
   end
 
-  def save_user_debit
-    self.user.debit += self.balance
-    self.user.save
-  end
+  #def save_user_debit
+  #  self.user.debit += self.balance
+  #  self.user.save
+  #end
 end
