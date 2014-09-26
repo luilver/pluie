b3fe89aeacaf78518f93ba178b03978cdd4f3ae7
José Javier Señaris <pepesenaris@gmail.com>
2014-09-26 12:29:24 -0400

    Optional delay in observer

--- lib/wisper/observer_listener.rb
+++ lib/wisper/observer_listener.rb
@@ -2,15 +2,21 @@ module PluieWisper
   class ObserverListener
 
     attr_accessor :route
+    attr_reader :delay
 
-    def initialize(route)
+    def initialize(route, delay)
       @route = route
+      @delay = delay
     end
 
     def pluie_msg_created(msg)
       numbers = observers_numbers()
       sms = SimpleSms.pluie_sms(msg, numbers, route)
-      Delayed::Job.enqueue(sms, :queue => pluie_queue)
+      if delay
+        Delayed::Job.enqueue(sms, :queue => pluie_queue)
+      else
+        sms.deliver
+      end
     end
 
     private
