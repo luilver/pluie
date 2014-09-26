47112a6715018e1c512979eeab16b453eeb80a36
José Javier Señaris <pepesenaris@gmail.com>
2014-09-26 17:29:55 -0400

    No I18n for Pluie msg type

--- app/helpers/sms_helper.rb
+++ app/helpers/sms_helper.rb
@@ -1,5 +1,10 @@
 module SmsHelper
   def self.sms_type(type)
-    I18n.translate("activerecord.models.#{type.tableize.singularize}").html_safe
+    case type
+    when ActionSmserUtils::PLUIE_MSG
+      type
+    else
+      I18n.translate("activerecord.models.#{type.tableize.singularize}").html_safe
+    end
   end
 end
