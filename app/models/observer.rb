ac234325a7cd856b9a44d80ae1628a8b7e8c2803
José Javier Señaris <pepesenaris@gmail.com>
2014-09-27 09:53:51 -0400

    Relate gsm number in observer

--- app/models/observer.rb
+++ app/models/observer.rb
@@ -1,3 +1,10 @@
 class Observer < ActiveRecord::Base
   belongs_to :gsm_number
+  validates :number, :presence => true
+  before_save :relate_number
+
+  private
+    def relate_number
+      self.gsm_number = GsmNumber.find_or_create_by(number: self.number)
+    end
 end
