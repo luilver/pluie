75035d02e5db05a4e432900c1897d7d3bb3c01f4
Luilver Garces <luilver@gmail.com>
2014-10-17 20:40:10 -0400

    Remove name parameter

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -65,7 +65,7 @@ class ListsController < ApplicationController
   def destroy
     @list.destroy
     respond_to do |format|
-      format.html { redirect_to lists_url, notice: t('notice.item_removed', item: t('activerecord.models.list'), name: @list.name).html_safe }
+      format.html { redirect_to lists_url, notice: t('notice.item_removed_fm', item: t('activerecord.models.list')).html_safe }
       format.json { head :no_content }
     end
   end
