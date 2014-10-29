e1103103c60910b3dfffddbda34ec18ab541ab48
José Javier Señaris <pepesenaris@gmail.com>
2014-10-29 13:14:48 -0400

    Fix params order in cashier

--- lib/wisper/cashier.rb
+++ lib/wisper/cashier.rb
@@ -4,7 +4,7 @@ module PluieWisper
     def finish_sending(sms, succesfull_numbers)
       bill = sms.pluie_message_id && Bill.find_by_message_id(sms.pluie_message_id)
       if bill
-        bill.update_and_charge_if_ended(sms.route_id, succesfull_numbers, sms.body_parts)
+        bill.update_and_charge_if_ended(succesfull_numbers, sms.route_id, sms.body_parts)
       end
     end
   end
