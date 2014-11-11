f2b9f782bb55618d702de70fc556438021d1fccc
José Javier Señaris <pepesenaris@gmail.com>
2014-11-10 19:26:39 -0500

    Include locale in urls during testing

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,3 +11,12 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
 end
+
+#to set locale in url build during tests
+class ActionController::TestCase
+  def process_with_default_locale(action, http_method = 'GET', parameters = nil, session = nil, flash = nil)
+    parameters = {:locale=> I18n.locale }.merge(parameters||{})
+    process_without_default_locale(action, http_method,  parameters, session, flash)
+  end
+  alias_method_chain :process, :default_locale
+end
