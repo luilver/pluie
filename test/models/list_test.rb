34bdaa3afc63787f086ef386e6256622ab859ba2
José Javier Señaris <pepesenaris@gmail.com>
2014-11-19 12:15:29 -0500

    Validates list paperclip field

--- test/models/list_test.rb
+++ test/models/list_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class ListTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  should have_attached_file :file
+  should validate_attachment_content_type(:file).allowing('text/plain')
+  should validate_attachment_presence(:file)
 end
