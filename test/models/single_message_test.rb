21ae14d243421e491edd0a02434b57f4266d21da
José Javier Señaris <pepesenaris@gmail.com>
2014-11-21 16:15:27 -0500

    First tests on single message

--- test/models/single_message_test.rb
+++ test/models/single_message_test.rb
@@ -1,6 +1,13 @@
 require 'test_helper'
 
 class SingleMessageTest < ActiveSupport::TestCase
+
+  setup do
+    @one = single_messages(:one)
+    @numbers = []
+    5.times{ |i| @numbers << cubacel_random_number }
+  end
+
   test "should have message" do
     sm = SingleMessage.random
     assert_not sm.message.blank?
@@ -10,4 +17,18 @@ class SingleMessageTest < ActiveSupport::TestCase
     sm = SingleMessage.random
     assert_not sm.number.blank?
   end
+
+  test "receivers with multiple numbers" do
+    @one.number = @numbers.join(" ")
+    @numbers.each do |num|
+      assert_includes @one.receivers, num
+    end
+  end
+
+  test "gsm_numbers_count equals receivers count" do
+    assert_equal @one.receivers.count, @one.gsm_numbers_count
+    @one.number = @numbers.join(" ")
+    assert_equal @one.receivers.count, @one.gsm_numbers_count
+  end
+
 end
