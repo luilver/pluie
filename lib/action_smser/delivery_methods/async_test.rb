77cd7c3c2020ef001d4829b84949d71788384679
José Javier Señaris <pepesenaris@gmail.com>
2014-11-22 00:08:25 -0500

    Initial setup in AsyncTest DLV Method

--- lib/action_smser/delivery_methods/async_test.rb
+++ lib/action_smser/delivery_methods/async_test.rb
@@ -3,5 +3,24 @@ require_relative 'async_http'
 module ActionSmser::DeliveryMethods
   class AsyncTest < AsyncHttp
 
+    def self.base_url
+      "http://pluietests.local/"
+    end
+
+    def self.path_url
+      "mocktests"
+    end
+
+    def self.gateway_key
+      :test
+    end
+
+    def self.sms_info(sms)
+      {:text => sms.body}
+    end
+
+    def self.request_params(info, numbers, sms)
+      info.merge({:destination => numbers.join(",")})
+    end
   end
 end
