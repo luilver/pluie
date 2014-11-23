8b405d3ec586b6386c9ad31c20e150ee61fe100e
José Javier Señaris <pepesenaris@gmail.com>
2014-11-23 16:53:36 -0500

    Async test DLR

--- lib/action_smser/delivery_methods/async_test.rb
+++ lib/action_smser/delivery_methods/async_test.rb
@@ -1,10 +1,16 @@
 require_relative 'async_http'
+require 'json'
+require 'securerandom'
 
 module ActionSmser::DeliveryMethods
   class AsyncTest < AsyncHttp
 
+    def self.full_url
+      "#{base_url}/#{path_url}"
+    end
+
     def self.base_url
-      "http://pluietests.local/"
+      "http://localhost"
     end
 
     def self.path_url
@@ -19,8 +25,33 @@ module ActionSmser::DeliveryMethods
       {:text => sms.body}
     end
 
-    def self.request_params(info, numbers, sms)
-      info.merge({:destination => numbers.join(",")})
+    def self.request_body(info, numbers, sms)
+      info.merge({:numbers => numbers.to_a}).to_json
+    end
+
+    def self.request_options( query_params, body, keepalive)
+      options = super(query_params, body, keepalive)
+      options.merge!({head: {'content-type' => 'application/json', "accept" => "*/*"}})
+    end
+
+    def self.parse_response(response)
+      res = JSON.parse(response)
+    end
+
+    def self.save_delivery_reports(sms, results, user, route_name)
+      count = 0
+      results.each do |result|
+        count += 1
+        dr = ActionSmser::DeliveryReport.build_with_user(sms, result["number"], result["id"], user, route_name)
+        dr.save
+      end
+      count
+    end
+
+    def self.generate_response_from_body(body)
+      data = JSON.parse(body)
+      res = data["numbers"].map {|num| {number: num, id: SecureRandom.uuid()}}
+      res.to_json
     end
   end
 end
