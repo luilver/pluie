4e2651978862bc1768516f66419a226754c31529
José Javier Señaris <pepesenaris@gmail.com>
2014-11-26 22:48:27 -0500

    Add choose_random to ActiveRecord::Base

--- config/initializers/custom.rb
+++ config/initializers/custom.rb
@@ -1,1 +1,8 @@
 require 'observer_sms_job'
+
+class ActiveRecord::Base
+  def self.choose_random
+    constant = self.to_s.constantize
+    constant.send(:all)[rand constant.send(:count)]
+  end
+end
