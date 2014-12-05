982a17bb9de01991f381f7ae3b2f1b01b04bf0fd
José Javier Señaris <pepesenaris@gmail.com>
2014-12-05 11:50:20 -0500

    GsmNumber must have cubacel format

--- app/models/gsm_number.rb
+++ app/models/gsm_number.rb
@@ -5,6 +5,8 @@ class GsmNumber < ActiveRecord::Base
   has_and_belongs_to_many :bulk_messages
   has_one :observer
 
+  validates :number, format: { with: /535[0-9]{7}/, message: I18n.t('errors.messages.valid_gsm_number_format').html_safe}
+
   def self.find_by_number_or_create(num)
     GsmNumber.find_by_number(num) || GsmNumber.create(:number => num)
   end
