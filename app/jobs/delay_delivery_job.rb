1fabbf6019cb819d03165f444571bea5b4d1a978
José Javier Señaris <pepesenaris@gmail.com>
2014-12-08 17:26:00 -0500

    Use MessageProcessor in delay delivery job

--- app/jobs/delay_delivery_job.rb
+++ app/jobs/delay_delivery_job.rb
@@ -1,9 +1,11 @@
-DelayDeliveryJob = Struct.new(:message_class, :msg_id) do
+DelayDeliveryJob = Struct.new(:message_class, :msg_id, :deliverer, :decorators) do
   def perform
     begin
       klass = message_class.constantize
+      dlv = deliverer.constantize
       msg = klass.find(msg_id)
-      msg.deliver
+      decs = decorators.map { |d| d.constantize }
+      MessageProcessor.deliver(msg, dlv, decs)
     rescue Exception => e
       Rails.logger.info "#{klass} with id #{msg_id} not found. Error: #{e.message}"
     end
