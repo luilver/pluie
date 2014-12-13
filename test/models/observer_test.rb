6d69592297f64a9771bf7625bac56a70897fc81b
José Javier Señaris <pepesenaris@gmail.com>
2014-12-13 10:13:26 -0500

    Shoulda validations in observer gsm_number

--- test/models/observer_test.rb
+++ test/models/observer_test.rb
@@ -1,27 +1,14 @@
 require 'test_helper'
 
 class ObserverTest < ActiveSupport::TestCase
+  should validate_presence_of :number
+  should allow_value("5354231267").for(:number)
 
   setup do
     #to run callback on gsm_number
     Observer.all.each do |single|
       single.save
     end
-  end
-
-  test "should have number" do
-    ob = Observer.new()
-    assert_not ob.save
-    ob.number =  cubacel_random_number
-    assert ob.save
-  end
-
-  test "should be a cubacel number" do
-    ob = Observer.new(number: "2123234131")
-    assert ob.invalid?
-    assert_includes ob.errors[:number], I18n.t('errors.messages.valid_gsm_number_format').html_safe
-    ob.number = cubacel_random_number
-    assert ob.save
   end
 
   test "should obtain active observers" do
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -9,13 +9,13 @@ class UserTest < ActiveSupport::TestCase
 
   test "credits sum should equals user credit" do
     user = users(:without_credits)
-    10.times{ |i| Credit.create(balance: @rnd.rand(1..10), user: user, description: "test") }
+    5.times{ |i| Credit.create(balance: @rnd.rand(1..10), user: user, description: "test") }
     assert_equal user.credits.to_a.sum {|c| c.balance}, user.credit
   end
 
   test "debits sum should equals user debit" do
     user = users(:without_credits)
-    10.times{ |i| Debit.create(balance: @rnd.rand(1..10), user: user) }
+    5.times{ |i| Debit.create(balance: @rnd.rand(1..10), user: user) }
     assert_equal user.debits.to_a.sum {|c| c.balance}, user.debit
   end
 
