2096f252d64b8fd9c0e611266da4bad0352b33ae
José Javier Señaris <pepesenaris@gmail.com>
2014-12-16 16:29:20 -0500

    CanCan perms for ActionSmser::DeliveryReport

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -19,8 +19,7 @@ class Ability
       can :index, Credit, :user_id => user.id
       cannot [:create, :update, :destroy, :show], Credit
 
-      can :read, ActionSmser::DeliveryReport, :user_id => user.id
-      can :list, ActionSmser::DeliveryReport, :user_id => user.id
+      can [:read, :index, :summary], ActionSmser::DeliveryReport, :user_id => user.id
       cannot [:create, :update, :destroy], ActionSmser::DeliveryReport
 
       can :balance, User, :user_id => user.id
