0ddcc8233dbd9bdb103162aacd96f1d9a841f492
José Javier Señaris <pepesenaris@gmail.com>
2014-12-16 16:32:08 -0500

    Check availables locales & main_app.root_url in App controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,18 +10,17 @@ class ApplicationController < ActionController::Base
   end
 
   rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_url, :alert => I18n.t('cancan.access_denied').html_safe
+    redirect_to main_app.root_url, :alert => I18n.t('cancan.access_denied').html_safe
   end
 
   before_action :set_locale
 
   def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
+    l = params[:locale] if I18n.available_locales.map(&:to_s).include?(params[:locale])
+    I18n.locale = l || I18n.default_locale
   end
 
   def default_url_options(options = {})
     {locale: I18n.locale}
   end
-
-
 end
