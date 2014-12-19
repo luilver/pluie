7d4709c88ea9cc96b80b550ed45dba20e2323b94
José Javier Señaris <pepesenaris@gmail.com>
2014-12-19 15:35:06 -0500

    GsmNumber id to Contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,6 +2,7 @@ class Contact < ActiveRecord::Base
   belongs_to :user
   has_and_belongs_to_many :groups
   has_and_belongs_to_many :gsm_numbers
+  belongs_to :main_number, class_name: 'GsmNumber', foreign_key: 'gsm_number_id'
 
   def to_s
     self.name
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141214025157) do
+ActiveRecord::Schema.define(version: 20141219182407) do
 
   create_table "action_smser_delivery_reports", force: true do |t|
     t.string   "msg_id"
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(version: 20141214025157) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.integer  "gsm_number_id"
   end
 
   create_table "contacts_groups", id: false, force: true do |t|
