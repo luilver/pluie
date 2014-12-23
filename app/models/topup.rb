e8907e4bc95aa2e68d5e27f352729873dded97dc
Luilver Garces <luilver@gmail.com>
2014-12-23 16:15:55 -0500

    Inline fields

--- app/models/topup.rb
+++ app/models/topup.rb
@@ -1,3 +1,15 @@
 class Topup < ActiveRecord::Base
   belongs_to :user
+
+  RECHARGE_TYPES = [
+    "10",
+    "15",
+    "20",
+    "25",
+    "30",
+    "35",
+    "40",
+    "45",
+    "50",
+  ]
 end
