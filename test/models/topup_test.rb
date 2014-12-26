02b2fa8740bb077558f962a072f199e48b4dc494
José Javier Señaris <pepesenaris@gmail.com>
2014-12-25 20:46:16 -0500

    Topups model tests

--- test/models/topup_test.rb
+++ test/models/topup_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 class TopupTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  should validate_presence_of :number
+  should allow_value(cubacel_random_number).for(:number)
+  should_not allow_value("54021461").for(:number)
+
+  setup do
+    @api_client = TopupApiService.new
+    @api_client.stubs(:current_ticket).returns("ticket")
+  end
+
+  test "should charge topups to user" do
+    u = users(:two)
+    topups = u.topups
+    price_strategy = TopupCashier.default_instance.price_strategy
+    total_price = topups.to_a.reduce(0) { |sum, t| sum + price_strategy.calculate_price(t)  }
+    @api_client.stubs(:call_and_fail_gracefully).returns({code: 1, message: "recarga exitosa"})
+    assert_differences [['User.find(u.id).balance', -total_price], ['User.find(u.id).debits.count', topups.count]] do
+      RechargePhonesService.execute(topups, @api_client)
+    end
+  end
+
+  test "should not charge failed topups" do
+    topup = topups(:one)
+    user_id = topup.user.id
+    @api_client.stubs(:call_and_fail_gracefully).returns({code: 0, message: "service unavailable"})
+    assert_difference ['User.find(user_id).balance', 'User.find(user_id).debits.count'], 0 do
+      RechargePhonesService.execute(topups, @api_client)
+    end
+  end
 end
