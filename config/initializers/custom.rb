455d77111505b39c772f4e475198182233037759
José Javier Señaris <pepesenaris@gmail.com>
2014-12-30 14:22:32 -0500

    Choose random AR object fix

--- config/initializers/custom.rb
+++ config/initializers/custom.rb
@@ -3,6 +3,6 @@ require 'observer_sms_job'
 class ActiveRecord::Base
   def self.choose_random
     constant = self.to_s.constantize
-    constant.send(:all)[rand constant.send(:count)]
+    constant.limit(1).offset(rand(constant.count)).first
   end
 end
