f37bda5977c76aef1de954b36cd21e0e01888c59
José Javier Señaris <pepesenaris@gmail.com>
2014-12-29 21:22:25 -0500

    Use TopupApiService directly in topup tests

--- test/models/topup_test.rb
+++ test/models/topup_test.rb
@@ -8,17 +8,17 @@ class TopupTest < ActiveSupport::TestCase
   setup do
     @api_client = TopupApiService.new
     @api_client.stubs(:current_ticket).returns("ticket")
+    @api_client.subscribe(TopupCashier.new,  on: :topup_api_recharge_success, with: :charge)
   end
 
   test "should charge topups to user" do
     u = users(:two)
     topups = u.topups
-    price_strategy = TopupCashier.default_instance.price_strategy
+    price_strategy = TopupCashier.new.price_strategy
     total_price = topups.to_a.reduce(0) { |sum, t| sum + price_strategy.calculate_price(t)  }
     @api_client.stubs(:call_and_fail_gracefully).returns({code: 1, message: "recarga exitosa"})
     assert_differences [['User.find(u.id).balance', -total_price], ['User.find(u.id).debits.count', topups.count]] do
-      recharge_service = RechargePhonesService.new(@api_client)
-      recharge_service.execute(topups)
+      @api_client.recharge(topups)
     end
   end
 
@@ -27,8 +27,7 @@ class TopupTest < ActiveSupport::TestCase
     user_id = topup.user.id
     @api_client.stubs(:call_and_fail_gracefully).returns({code: 0, message: "service unavailable"})
     assert_difference ['User.find(user_id).balance', 'User.find(user_id).debits.count'], 0 do
-      recharge_service = RechargePhonesService.new(@api_client)
-      recharge_service.execute(topups)
+      @api_client.recharge(topups)
     end
   end
 end
