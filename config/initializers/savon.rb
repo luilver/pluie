130c70b17212e4ea4d0dab0c812333a1fa8ac08b
José Javier Señaris <pepesenaris@gmail.com>
2015-01-03 12:19:45 -0500

    Remove global & local info in Savon Observer

--- config/initializers/savon.rb
+++ config/initializers/savon.rb
@@ -5,10 +5,6 @@ class SavonObserver
   def notify(operation_name, builder, globals, locals)
     Rails.logger.info "Savon request: operation: #{operation_name}"
     Rails.logger.info "builder: #{builder.pretty}"
-    Rails.logger.info "globals:"
-    Rails.logger.info  "#{globals.inspect}"
-    Rails.logger.info "locals:"
-    Rails.logger.info  "#{locals.inspect}"
     nil
   end
 
