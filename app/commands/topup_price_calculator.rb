95b04da3956e813e3f3db8377cf24af3beab45da
José Javier Señaris <pepesenaris@gmail.com>
2015-01-06 22:01:27 -0500

    Round up topup api default prices

--- app/commands/topup_price_calculator.rb
+++ app/commands/topup_price_calculator.rb
@@ -6,13 +6,16 @@ class TopupPriceCalculator
   end
 
   def initialize(prices= {}, pluie_fee= 1.1)
-    @fixed_prices = (((10..50).step(5).map { |a| [ a, a * pluie_fee] }).to_h).merge(prices)
+    @fixed_prices = (((10..50).step(5).map { |a| [ a, (a * pluie_fee).round(1)] }).to_h).merge(prices)
     @fixed_prices.freeze
     @pluie_fee = pluie_fee
   end
 
   def calculate_price(topup)
-    amount = topup.amount
+    calculate_price_by_amount(topup.amount)
+  end
+
+  def calculate_price_by_amount(amount)
     fixed_prices[amount] || amount * pluie_fee
   end
 end
