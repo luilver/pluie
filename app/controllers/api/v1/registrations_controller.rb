46d3733f3345e1f553d9f029c043796bba85d85a
alejandro herrera nogueira <alehn96@gmail.com>
2015-10-20 22:52:29 -0400

    create method authenticate_admin! for registration user

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -3,6 +3,7 @@ module Api
 
   class RegistrationsController < Devise::RegistrationsController
     protect_from_forgery with: :null_session, :if => Proc.new { |c| c.request.format == 'application/api.knales.v' }
+    before_filter :authenticate_admin!
     respond_to :json
 
     def create
@@ -21,7 +22,25 @@ module Api
       params.require(:sign_up_params).permit(:email, :password, :password_confirmation)
     end
 
-
+    def authenticate_admin!
+      if not (request.headers["HTTP_EMAIL"].blank? || request.headers["HTTP_API_KEY"].blank?)
+         if user=User.find_by_email(request.headers["HTTP_EMAIL"])
+           if Devise.secure_compare(user.api_key,  request.headers["HTTP_API_KEY"])
+             if user.admin
+               return
+             else
+               render json: {:message=>"the user is not admin"},status: 401
+             end
+           else
+             render json: {:message=>"api_key invalid"}, status: 401
+           end
+         else
+          render json: {:message=>"email invalid"}, status: 400
+         end
+      else
+        render json: {:message=>"missing email or api_key"},status: 404
+      end
+    end
   end
 end
 end
