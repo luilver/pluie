9cee8f1eb5a033a3cda4518cb0b2849b176e584c
Alejandro.herrera <alehn96@gmail.com>
2015-10-21 13:36:02 -0400

    action create, index, show, and balance done in controller user api-rest

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,19 +5,42 @@ module Api
       respond_to :json
 
       def balance
-        respond_with User.current.balance
+        render json: {:user=>{:email=>User.current.email, :balance=>User.current.balance}},status: 200
       end
 
       def index
-        respond_with User.all
+        if User.current.admin
+          render json: {:users=>User.all.map{|u| {:email=>u.email,:balance=>u.balance,:Gasto_Total=>u.spent}}},status: 200
+        else
+          render json: {:message=>"you don't have permission for this resource"},status: 401
+        end
       end
 
       def show
-        respond_with User.find(params[:id])
+        if User.current.admin
+          if not User.find_by_id(params[:id]).blank?
+            render json: {:user=>{:email=>User.find_by_id(params[:id]).email,:balance=>User.find_by_id(params[:id]).balance}},status: 200
+          else
+            render json: {:message=>'Not exists user with that id'},status: 404
+          end
+        else
+            render json: {:message=>"you don't have permission for this resource"},status: 401
+        end
       end
 
       def create
-        respond_with User.create(params[:user])
+        if User.current.admin
+          @user=User.new(user_params)
+          @user.confirmed_at=DateTime.now
+
+          if @user.save
+           render json: {:message=>"The user was created succefully"}, status: 201
+          else
+            render json: @user.errors, status: :unprocessable_entity
+          end
+        else
+          render json: {:message=>"you don't have permission for this resource"},status: 401
+        end
       end
 
       def update
@@ -26,6 +49,12 @@ module Api
 
       def destroy
         respond_with User.destroy(params[:id])
+      end
+
+      protected
+
+      def user_params
+        params.require(:user).permit(:email, :password, :password_confirmation, :admin, :credit, :locale)
       end
     end
   end
