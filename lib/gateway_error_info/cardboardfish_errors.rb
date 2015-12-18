08d9c6a1b3aaff71fe00c8f5f78e4258a37a2fa2
alejandro herrera nogueira <alehn96@gmail.com>
2015-12-18 02:31:31 -0500

    comment in cardboardfish errors

--- lib/gateway_error_info/cardboardfish_errors.rb
+++ lib/gateway_error_info/cardboardfish_errors.rb
@@ -1,6 +1,6 @@
 module GatewayErrorInfo
   module CardboardfishErrors
-    SUCCESS_CODE = 1
+    SUCCESS_CODE = 1 #puesto por mi
 
     HTTP_ERROR_CODES = {
         -5=>"Not_Enough_Credit",
@@ -8,7 +8,6 @@ module GatewayErrorInfo
         -15=>"Invalid_destination_or_destination_not_covered",
         -20=>"System_error_",
         -25=>"Request_Error"
-
     }
     module ClassMethods
       def cardboardfish_error(error_code)
