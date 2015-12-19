8dbcf99bda765733a89799715b1e11901b107fb8
alejandro herrera nogueira <alehn96@gmail.com>
2015-12-18 23:53:49 -0500

    add require fortytwo in lib/gateway_error

--- lib/gateway_error_info.rb
+++ lib/gateway_error_info.rb
@@ -1,6 +1,7 @@
 require 'gateway_error_info/infobip_errors'
 require 'gateway_error_info/routesms_errors'
 require 'gateway_error_info/nexmo_errors'
+require 'gateway_error_info/fortytwo_errors'
 
 module GatewayErrorInfo
 end
