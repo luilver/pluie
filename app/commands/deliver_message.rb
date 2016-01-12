94f04154f4406fc4e069f6091adf8cf0ff4ba388
alejandro herrera nogueira <alehn96@gmail.com>
2016-01-12 16:12:32 -0500

    put algorithm for backups in deliver_message.rb

--- app/commands/deliver_message.rb
+++ app/commands/deliver_message.rb
@@ -9,9 +9,30 @@ class DeliverMessage
     end
   end
 
-  def deliver(message)
-    strategy.deliver(message)
-    publish(:message_delivery, message)
+  def deliver(message,backupSms)
+     if backupSms
+       strategy.deliver(message)
+       publish(:message_delivery, message)
+
+       routes= message.user.routes.where.not(:name=>message.route.name)
+       time=10
+       routes.each do |r|
+              ScheduleSms::SCHEDULER.in (time.to_s + 'm') do
+              #if status dont send (tengo que poner esto)
+              if true
+                message.route=r
+                strategy.deliver(message)
+                publish(:message_delivery, message)
+              else
+                return
+              end
+            end
+         time=time+10
+       end
+     else
+      strategy.deliver(message)
+      publish(:message_delivery, message)
+     end
   end
 
 end
