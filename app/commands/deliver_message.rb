0b73bd272de484651fb5617009394517c1b086b9
alejandro herrera nogueira <alehn96@gmail.com>
2016-01-13 16:59:42 -0500

    deliver message start algorithm of backup

--- app/commands/deliver_message.rb
+++ app/commands/deliver_message.rb
@@ -9,9 +9,20 @@ class DeliverMessage
     end
   end
 
-  def deliver(message,backupSms)
-      strategy.deliver(message)
-      publish(:message_delivery, message)
-
+  def deliver(message,backupSms=false)
+      if backupSms
+       if message.class == SingleMessage
+         strategy.deliver(message)
+         publish(:message_delivery, message)
+         if message.user.routes.count > 1
+           rt=message.user.routes.order(price: :asc).select{|r| r.id!=message.route.id}
+           job= SendSmsBackupJob.new(message, rt) #nuevo message y una ruta menos
+           Delayed::Job.enqueue(job,:run_at => 5.minutes.from_now) #manda a ejecutarlo dentro de 5minutos
+         end
+       end
+      else
+        strategy.deliver(message)
+        publish(:message_delivery, message)
+      end
   end
 end
