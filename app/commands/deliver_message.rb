c5ae82d346d2d58792f0eb73e79b66c1651b7983
alejandro herrera nogueira <alehn96@gmail.com>
2016-01-14 11:47:34 -0500

    checking delivered previous messages

--- app/commands/deliver_message.rb
+++ app/commands/deliver_message.rb
@@ -16,7 +16,8 @@ class DeliverMessage
          publish(:message_delivery, message)
          if message.user.routes.count > 1
            rt=message.user.routes.order(price: :asc).select{|r| r.id!=message.route.id}
-           job= SendSmsBackupJob.new(message, rt) #nuevo message y una ruta menos
+           listsmessages=[message]
+           job= SendSmsBackupJob.new(listsmessages, rt) #nuevo message y una ruta menos
            Delayed::Job.enqueue(job,:run_at => 5.minutes.from_now) #manda a ejecutarlo dentro de 5minutos
          end
        end
--- app/jobs/send_sms_backup_job.rb
+++ app/jobs/send_sms_backup_job.rb
@@ -1,11 +1,18 @@
 require 'action_smser_utils'
 
-SendSmsBackupJob = Struct.new(:message, :routes) do
+SendSmsBackupJob = Struct.new(:list_messages, :routes) do
   def perform
     begin
-      dr = ActionSmser::DeliveryReport.where(:pluie_id => message.id.to_s).first
-      if dr.status != ActionSmserUtils::DELIVERED_STATUS
-        if routes.count >0
+      deliver=false
+      list_messages.each do |message|
+        dr = ActionSmser::DeliveryReport.where(:pluie_id => message.id.to_s).first
+        if  dr.status == ActionSmserUtils::DELIVERED_STATUS
+          deliver=true
+        end
+      end
+
+      if !deliver
+        if routes.count > 0
           sm=SingleMessage.new
           sm.number=message.number
           sm.message=message.message
@@ -13,10 +20,11 @@ SendSmsBackupJob = Struct.new(:message, :routes) do
           sm.route=routes.first
           sm.save
           SingleDeliverer.deliver(sm)
+          list_messages << sm
 
           if routes.count>1
-          job= SendSmsBackupJob.new(sm, routes.last(routes.count-1)) #nuevo message y una ruta menos
-          Delayed::Job.enqueue(job,:run_at => 5.minutes.from_now) #manda a ejecutarlo dentro de 5minutos
+            job= SendSmsBackupJob.new(list_messages, routes.last(routes.count-1)) #nuevo message y una ruta menos
+            Delayed::Job.enqueue(job,:run_at => 5.minutes.from_now) #manda a ejecutarlo dentro de 5minutos
           end
         end
       end
