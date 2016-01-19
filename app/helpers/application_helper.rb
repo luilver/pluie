a75b805f2b071d24798d0ae6b96420e66ca6c906
alejandro herrera nogueira <alehn96@gmail.com>
2016-01-19 13:08:49 -0500

    add class schedule_utils in helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  class ScheduleUtils
+    def self.schedule(job,t)
+      #donde t es un objeto de tipo Time y job obj de tipo ScheduleSmsJob
+      Delayed::Job.enqueue(job,:run_at=>t)
+    end
+  end
 end
