b56ae1db5a652757257c57c501e78e1bcae603a1
alejandro herrera nogueira <alehn96@gmail.com>
2016-01-19 13:26:33 -0500

    set default argument  in obj Time

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   class ScheduleUtils
-    def self.schedule(job,t)
+    def self.schedule(job,t = Time.now)
       #donde t es un objeto de tipo Time y job obj de tipo ScheduleSmsJob
       Delayed::Job.enqueue(job,:run_at=>t)
     end
