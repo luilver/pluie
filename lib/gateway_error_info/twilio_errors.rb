3f362c5895830b9fab76085885d57c32d261a065
alejandro herrera nogueira <alehn96@gmail.com>
2016-03-18 12:01:21 -0400

    adds error code

--- lib/gateway_error_info/twilio_errors.rb
+++ lib/gateway_error_info/twilio_errors.rb
@@ -1,10 +1,22 @@
 module GatewayErrorInfo
   module TwilioErrors
 
+    HTTP_ERROR_CODES = {
+        30001  => "Queue_overflow",
+        30002  => "Account_suspended",
+        30003  => "Unreachable_destination_handset",
+        30004  => "Message_blocked",
+        30005  => "Unknown_destination_handset",
+        30006  => "Landline_or_unreachable_carrier",
+        30007  => "Carrier_violation",
+        30008  => "Unknown_error",
+        30009  => "Missing_segment",
+        30010  => "Message_price_exceeds_max_price"
+    }
     module ClassMethods
       def twilio_error(error_code)
         #TODO: add smpp error. according
-         "UNKNOWN_ERROR"
+        HTTP_ERROR_CODES[error_code] || "UNKNOWN_ERROR"
       end
 
     end
