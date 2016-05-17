6c69dd32035a89c5c0d12f4f837ea4af5db7da41
alejandro herrera nogueira <alehn96@gmail.com>
2016-05-17 00:44:25 -0400

    added form and query for prefixtable

--- app/controllers/prefix_controller.rb
+++ app/controllers/prefix_controller.rb
@@ -1,10 +1,19 @@
 class PrefixController < ApplicationController
 
+  def index
+    @prefix_all  = PrefixTable.paginate(:page => params[:page], :per_page => 5)
+  end
+
   def new
     @prefix_t = PrefixTable.new
   end
 
+  def edit
+    @prefix_t=PrefixTable.find(params[:id])
+  end
+
   def show
+    @prefix_t=PrefixTable.find(params[:id])
   end
 
   def create
@@ -16,4 +25,20 @@ class PrefixController < ApplicationController
    end
   end
 
+  def update
+    @prefix_t= PrefixTable.find(params[:id])
+    @prefix_t.country_code=params[:prefix]
+    @prefix_t.country=params[:country]
+    if @prefix_t.save
+      render 'show'
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @prefix_t=PrefixTable.find(params[:id])
+    @prefix_t.destroy
+    redirect_to prefix_path
+  end
 end
--- app/models/prefix_table.rb
+++ app/models/prefix_table.rb
@@ -2,6 +2,9 @@ class PrefixTable < ActiveRecord::Base
   has_many :gateway_prefixtables, :dependent => :destroy
   has_many :gateways, -> {distinct}, :through => :gateway_prefixtables
   validates :country_code, :uniqueness => true
+  validates :country, :uniqueness => true
+  validates :country_code, :presence => true
+  validates :country, :presence => true
 
   def price_gateway(name_gateway)
     return nil if Gateway.find_by_name(name_gateway).blank?
