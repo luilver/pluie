db93f8d0a8f71535c561bd0df347f39d4fd121b0
alejandro herrera nogueira <alehn96@gmail.com>
2016-05-27 22:59:15 -0400

    added information in initial page

--- app/models/user.rb
+++ app/models/user.rb
@@ -68,4 +68,41 @@ class User < ActiveRecord::Base
   def self.search(search, page)
     paginate :per_page => 10,:page=>page,:conditions=>['email like ?', "%#{search}%"],:order=> {:created_at=>:desc}
   end
+
+  def query_home
+    return {:today=>count_message('today'),:h_48=>count_message('48_last_hour'),:t_week=>count_message('week'),:d_15=>count_message('15_last_days'),:t_month=>count_message('this_month'),:d_60=>count_message('60_last_days'),:t_year=>count_message('this_year'),:d_500=>count_message('500_last_day'),:l_hour=>count_message('last_hour')}
+  end
+
+  def count_message(date)
+    time_span = case date.to_s.downcase
+                  when '48_last_hour'
+                    48.hour.ago..Time.current
+                  when 'week'
+                    Time.current.at_beginning_of_week..Time.current
+                  when '15_last_days'
+                    15.days.ago..Time.current
+                  when 'this_month'
+                    Time.current.at_beginning_of_month..Time.current
+                  when '60_last_days'
+                    60.days.ago..Time.current
+                  when 'today'
+                    Time.current.at_beginning_of_day..Time.current
+                  when 'this_year'
+                    Time.current.at_beginning_of_year..Time.current
+                  when '500_last_day'
+                    500.days.ago..Time.current
+                  when 'last_hour'
+                    1.hour.ago..Time.current
+                  when /^(\d\d)-(\d\d) hours/
+                    $1.to_i.hours.ago..$2.to_i.hours.ago
+                  else
+                    1.week.ago..Time.current
+                end
+    if self.admin?
+          count_message= ActionSmser::DeliveryReport.where(:created_at => time_span).count
+    else
+          count_message= ActionSmser::DeliveryReport.where(:user_id=>self.id).where(:created_at => time_span).count
+    end
+    return count_message
+  end
 end
